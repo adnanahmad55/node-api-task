b0efc1f445e3f65103b708a34e0dbe87901ca4f7
adnanahmad55 <adnanahmed5575@gmail.com>
2026-01-12 11:28:18 +0530

    Node server ready for deployment

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const http = require("http");
 // INIT APP FIRST
 const app = express();
+const { Server } = require("socket.io");
 
 // MIDDLEWARES
 app.use(bodyParser.json());
@@ -27,14 +28,52 @@ mongoose
 
 // TEST ROUTE
 app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // SERVER
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+let liveUsers = [];
+io.on("connection", (socket) => {
+  console.log("🟢 User connected:", socket.id);
+
+socket.on("join_live_users", (user) => {
+  socket.join("live_users");
+
+  const alreadyExists = liveUsers.find(
+    (u) => u.email === user.email
+  );
+
+  if (!alreadyExists) {
+    liveUsers.push({
+      socketId: socket.id,
+      email: user.email,
+      name: user.firstName + " " + user.lastName,
+    });
+  }
+
+  io.to("live_users").emit("live_users_list", liveUsers);
+});
+
+
+  socket.on("disconnect", () => {
+    console.log("🔴 User disconnected:", socket.id);
+
+    liveUsers = liveUsers.filter(
+      (u) => u.socketId !== socket.id
+    );
+
+    io.to("live_users").emit("live_users_list", liveUsers);
+  });
+});
 const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server running with Socket.io on port ${PORT}`);
 });
