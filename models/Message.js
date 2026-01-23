fb45c4320cd1ea9b8c609469c0c6726e4dde1205
adnanahmad55 <adnanahmed5575@gmail.com>
2026-01-23 15:59:28 +0530

    initialmigration

--- models/Message.js
+++ models/Message.js
@@ -1,10 +1,10 @@
-// models/Message.js
 const mongoose = require("mongoose");
+
 const messageSchema = new mongoose.Schema({
     senderEmail: String,
     receiverEmail: String,
-    message: String,        // Text or caption
-    fileUrl: String,        // URL of image/video
+    message: String,
+    fileUrl: { type: String, default: null },
     messageType: { 
         type: String, 
         enum: ['text', 'image', 'video'], 
@@ -13,3 +13,6 @@ const messageSchema = new mongoose.Schema({
     delivered: { type: Boolean, default: false },
     read: { type: Boolean, default: false }
 }, { timestamps: true });
+
+// 🔥 YE LINE SABSE ZAROORI HAI:
+module.exports = mongoose.model("Message", messageSchema);
